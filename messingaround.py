cd9e144a7cd2960a687dcfd1beea3fb13d206f47
Parker Thurston <parker.thurston22@gmail.com>
2021-04-13 00:24:36 -0600

    dang bro, Progress? whats that

--- messingaround.py
+++ messingaround.py
@@ -1,34 +1,13 @@
 from pytube import YouTube 
-  
-# where to save 
-SAVE_PATH = "D:\College Stuff\Freshman\CS 2300\CS2300" #to_do 
-  
-# link of the video to be downloaded 
-link="https://www.youtube.com/watch?v=xWOoBJUqlbI"
-  
-try: 
-    # object creation using YouTube
-    # which was imported in the beginning 
-    yt = YouTube(link) 
-except: 
-    print("Connection Error") #to handle exception 
-  
-# filters out all the files with "mp4" extension 
 
-mp4files = yt.filter('mp4') 
-  
-#to set the name of the file
-yt.set_filename('GeeksforGeeks Video')  
-  
-# get the video with the extension and
-# resolution passed in the get() function 
-d_video = yt.get(mp4files[-1].extension,mp4files[-1].resolution) 
-try: 
-    # downloading the video 
-    d_video.download(SAVE_PATH) 
-except: 
-    print("Some Error!") 
-print('Task Completed!') 
+yt=YouTube('https://www.youtube.com/watch?v=Rvc-dn-JW_k')
+
+
+yt.streams.filter(adaptive=True, res="1080p").first().download()
+
+# .get_highest_resolution().download()
+
+
 
 
 
